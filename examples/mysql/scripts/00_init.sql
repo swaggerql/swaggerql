e3cd16d3e6278978559bd09603e3573660936b66
Pavlo Bashynskiy <levonet@gmail.com>
2019-10-02 15:58:10 +0300

    Add mysql2 support

---
+++ examples/mysql/scripts/00_init.sql
@@ -0,0 +1,12 @@
+CREATE USER 'myuser'@'%' IDENTIFIED WITH mysql_native_password BY 'secret';
+GRANT ALL PRIVILEGES ON *.* TO 'myuser'@'%';
+FLUSH PRIVILEGES;
+
+CREATE DATABASE petstore;
+USE petstore;
+
+CREATE TABLE IF NOT EXISTS pet (
+    pet_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    status ENUM ('available','pending','sold') NOT NULL
+) ENGINE=INNODB;
